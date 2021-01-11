795e5e5319d403e9563c4bd756781e78104f30d7
maklad42 <maklad42+git@gmail.com>
2021-01-11 22:35:11 +0900

    Initial commit. Install new Gatsby site. 210113

---
+++ src/pages/starwars.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const StarWarsAPI = () => {
+  ;<div className="starwars">
+    <h1> Different people and ships from the films</h1>
+  </div>
+}
+
+export default StarWarsAPI
