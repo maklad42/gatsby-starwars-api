ae39d3c80cb68d88e03fda166daf50fe0beb4c9f
maklad42 <maklad42+git@gmail.com>
2021-02-08 21:45:15 +0900

    Remove the broken code and start again. 210208

--- src/pages/starwars.js
+++ src/pages/starwars.js
@@ -3,19 +3,6 @@ const Swapi = () => (
   <div>
     <h1>People and ships from the films</h1>
   </div>
-)
-export default Swapi
+);
 
-export const query = graphql`
-  query allNodesSwapi {
-    allNodesSwapi {
-      edges {
-         node {
-           results{
-             name
-           }
-         }
-      }
-    }
-  }
-`
+export default Swapi
